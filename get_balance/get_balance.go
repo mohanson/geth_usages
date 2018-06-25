0189eafba88da6ea3b5f6d04403133a453a588df
mohanson <mohanson@outlook.com>
2018-06-25 19:36:52 +0800

    2018-06-25 19:36:52

--- get_balance/get_balance.go
+++ get_balance/get_balance.go
@@ -10,13 +10,13 @@ import (
 )
 
 func main() {
-	rpcDial, err := rpc.Dial("http://10.0.5.198:8547")
+	rpcDial, err := rpc.Dial("https://mainnet.infura.io")
 	if err != nil {
 		log.Fatalln(err)
 	}
 	client := ethclient.NewClient(rpcDial)
 
-	r, err := client.BalanceAt(context.Background(), common.HexToAddress("0x3aa9e3ab43f7149f3842d67863ecd80adba3447e"), nil)
+	r, err := client.BalanceAt(context.Background(), common.HexToAddress("0x0000000000000000000000000000000000000000"), nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
